f8ebcfab6a68d9d3ac07a79f8061a0be18a7129d
Ian Rich <iancarnation@gmail.com>
2014-02-03 17:28:20 -0800

    EOD 2/3/14
    Added stand alone working OpenGL setup

---
+++ OpenGLworks/OpenGLworks/include/Utilities.h
@@ -0,0 +1,12 @@
+#ifndef _UTILITIES_H_
+#define _UTILITIES_H_
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+unsigned int LoadTexture(const char* a_szTexture, unsigned int a_uiFormat /* = 
+GL_RGBA */, unsigned int* a_uiWidth /* = nullptr */, unsigned int* a_uiHeight /* = 
+nullptr */, unsigned int* a_uiBPP /* = nullptr*/);
+
+#endif
---
+++ OpenGLworks/OpenGLworks/source/Quad.cpp
@@ -0,0 +1,98 @@
+#include "Quad.h"
+
+Quad::Quad(void)
+{
+	// Default shaders for default constructor
+
+	const char* VertexShader = // Vertex Shaders deal with objects in 3D space
+		"#version 330\n"
+		"in vec3 position;"
+		"in vec4 color;"
+		//"in vec2 texcoord;"
+		"out vec4 vColor;"
+		"void main() {"
+		"	vColor = color;"
+		"	gl_Position = vec4 (position, 1.0);"
+		"}";
+
+	const char* FragmentShader = // Fragment Shader deals with pixel data
+		"#version 330\n"
+		"in vec4 vColor;"
+		//"in vec2 texcoord;"
+		"out vec4 outColor;"
+		"void main () {"
+		"	outColor = vColor;"
+		"}";
+
+	// Compile Vertex Shader
+	m_VertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(m_VertexShader, 1, &VertexShader, NULL);
+	glCompileShader(m_VertexShader);
+	printShaderInfoLog(m_VertexShader);
+
+	// Compile Fragment Shader
+	m_FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(m_FragmentShader, 1, &FragmentShader, NULL);
+	glCompileShader(m_FragmentShader);
+	printShaderInfoLog(m_FragmentShader);
+
+	// Link Shaders into Shader Program
+	m_ShaderProgram = glCreateProgram();
+
+	glAttachShader(m_ShaderProgram, m_FragmentShader);
+	glAttachShader(m_ShaderProgram, m_VertexShader);
+
+	glLinkProgram(m_ShaderProgram);
+	printProgramInfoLog(m_ShaderProgram);
+
+	glUseProgram(m_ShaderProgram);
+
+	// Create VAO
+	glGenVertexArrays(1, &m_VAO);
+	glBindVertexArray(m_VAO);
+
+	// Create BUffers
+	glGenBuffers(1, &m_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+
+	GLfloat points[] = 
+	{
+		-0.5f,  0.5f,  0.0f,  0.0f, 1.0f,  0.0f, 1.0,
+		 0.5f,  0.5f,  0.0f,  0.0f, 0.0f,  1.0f, 1.0,
+		-0.5f, -0.5f,  0.0f,  1.0f, 0.0f,  0.0f, 1.0,
+		 0.5f, -0.5f,  0.0f,  1.0f, 1.0f,  1.0f ,1.0
+	};
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+	// Specify layout of vertex data
+	GLint posAttrib = glGetAttribLocation(m_ShaderProgram, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 7*(sizeof(float)), 0);
+
+	GLint colAttrib = glGetAttribLocation(m_ShaderProgram, "color");
+	glEnableVertexAttribArray(colAttrib);
+	glVertexAttribPointer(colAttrib, 4, GL_FLOAT, GL_FALSE, 7*(sizeof(float)), (void*)(3 * sizeof(GLfloat)));
+
+	glGenBuffers(1, &m_EBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+		GLuint elements[] = 
+		{
+			0,1,2,3
+		};
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);		
+}
+
+Quad::~Quad()
+{}
+
+void Quad::Draw()
+{
+	glUseProgram(m_ShaderProgram);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	glBindVertexArray(m_VAO);
+
+	glDrawElements(GL_TRIANGLE_STRIP, 6, GL_UNSIGNED_INT, 0);
+}
+
+
